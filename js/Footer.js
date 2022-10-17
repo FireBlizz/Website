5e79a417e5fdad93189ada81432e96803171fc9b
FireBlizz <ktfixitboy@gmail.com>
2022-10-17 19:56:04 +1300

    init

---
+++ js/Footer.js
@@ -0,0 +1,3 @@
+//made for a constistant Footer for all pages that includes who owns the website and a link to a source page for all imagees used
+document.write('<footer><p>Website owned by Tecno-App<br>' +
+  '<a href="Source.html">Web-Map</a></p></footer>')
