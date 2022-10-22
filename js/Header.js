738f11607851a546ea9f5fed8c4d61884bf918fe
FireBlizz <ktfixitboy@gmail.com>
2022-10-22 17:06:46 +1300

    .

--- js/Header.js
+++ js/Header.js
@@ -1,5 +1,5 @@
-//made for a constistant Header for all pages that includes a logo for the home page aswell as a detail, booking and the about us page
+//made for a constistant Header for all pages that includes a logo for the home page aswell as a detail, ordering and the about us page
 document.write('<header><ul><li><a href="Home.html"><img src="images/icon.jpg" alt="logo" style="width:50px;height;50px;"></a></li>' +
   '<li><a href="Detail.html">Detail</a></li>' +
-  '<li><a href="user form.html">Booking</a></li>' + //TEMP, SWAP OUT "Home.html" FOR BOOKING
-  '<li><a href="About us.html">About Us</a></li></ul></header>') //TEMP, SWAP OUT "Detail.html" FOR ABOUT US
+  '<li><a href="user form.html">Order</a></li>' + 
+  '<li><a href="About us.html">About Us</a></li></ul></header>') 
