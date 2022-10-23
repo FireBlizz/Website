835d854218061cfacc01d71989e6f34e0b45815a
FireBlizz <ktfixitboy@gmail.com>
2022-10-23 21:38:43 +1300

    final2

--- js/Header.js
+++ js/Header.js
@@ -1,5 +1,5 @@
 //made for a constistant Header for all pages that includes a logo for the home page aswell as a detail, ordering and the about us page
 document.write('<header><ul class="jsHeaderUnorderd"><li class="jsHeaderListItem"><a class="jsHeaderInner" href="Home.html"><img src="images/icon.jpg" alt="logo" style="width:50px;height;50px;"></a></li>' +
   '<li class="jsHeaderListItem"><a class="jsHeaderInner" href="Detail.html">Detail</a></li>' +
-  '<li class="jsHeaderListItem"><a class="jsHeaderInner" href="user form.html">Order</a></li>' + 
+  '<li class="jsHeaderListItem"><a class="jsHeaderInner" href="order.html">Order</a></li>' + 
   '<li class="jsHeaderListItem"><a class="jsHeaderInner" href="About us.html">About Us</a></li></ul></header>') 
